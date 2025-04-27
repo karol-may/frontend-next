5296cd8daf037a9e83b12bf4e3ca0c8440d0c9de
Falbiak <falbiakisstupid@gmail.com>
2025-04-27 13:14:03 +0200

    Oda do handleSubjectSubmit'ości (#17)
    * Commitowanie to moje mniemanie
    
    * Oda do handleSubjectSubmit'ości
    
    ---------
    
    Co-authored-by: Karol May <karol@imay.pl>

--- app/subject/page.tsx
+++ app/subject/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { fetchSubjects } from "@/components/subject/action";
+import { addSubject, fetchSubjects } from "@/components/subject/action";
 import { useEffect, useState } from "react";
 import { Subject } from "@/types/Subject";
+import { Button } from "@/components/ui/button";
+import { useForm } from 'react-hook-form';
 import {
     Table,
     TableBody,
@@ -12,13 +14,18 @@ import {
     TableHeader,
     TableRow
 } from "@/components/ui/table"
+import {addCity} from "@/components/city/action";
 
-export default function SubjectPage()   {
-    const [subjects, setSubjects] = useState<Subject[]|null>(null);
+
+export default function SubjectPage() {
+    const [subjects, setSubjects] = useState<Subject[] | null>(null);
     const [error, setError] = useState<Error | null>(null);
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
     useEffect(() => {
         fetchSubjects()
-            .then(({ data, error }) => {
+            .then(({data, error}) => {
                 if (error) {
                     setError(error);
                 } else {
@@ -28,33 +35,44 @@ export default function SubjectPage()   {
             })
             .catch(otherError => setError(otherError)); // Obsługa nieoczekiwanych błędów
     }, []);
+
+    function handleSubjectSubmit(data: any) {
+        addSubject(data).then().catch(otherError => setError(null));
+    }
+
     return (
         <>
-
-            <Table>
-                <TableCaption>Informacje o dostępnych na stronie przedmiotach.</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[100px]">name</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead>Ilość nauczycieli</TableHead>
-                        <TableHead className="text-right">Ilość uczniów</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    { subjects?.map((subject, index) => (
-                        <TableRow key={index}>
-                            <TableCell className="font-medium">{subject.name}</TableCell>
-                            <TableCell>{subject.status?"Dostępne":"Strajk"}</TableCell>
-                            <TableCell>{subject.teachersNo}</TableCell>
-                            <TableCell className="text-right">{subject.studentsNo}</TableCell>
-                        </TableRow>
-                    ))}
-
-
-                </TableBody>
-            </Table>
+            {editMode ? (<h1>FORM</h1>) : (
+                <div className="flex flex-col space-y-4">
+                    <Button className={"block"} onClick={() => setEditMode(!editMode)}>Dodaj Przedmiot</Button>
+                    <Table>
+                        <TableCaption>Informacje o dostępnych na stronie przedmiotach.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">name</TableHead>
+                                <TableHead>Status</TableHead>
+                                <TableHead>Ilość nauczycieli</TableHead>
+                                <TableHead className="text-right">Ilość uczniów</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {
+                                subjects?.map((subject, index) => {
+                                    return (
+                                        <TableRow key={index}>
+                                            <TableCell className="font-medium">{subject.name}</TableCell>
+                                            <TableCell>{subject.status ? "Dostępne" : "Strajk"}</TableCell>
+                                            <TableCell>{subject.teachersNo}</TableCell>
+                                            <TableCell className="text-right">{subject.studentsNo}</TableCell>
+                                        </TableRow>
+                                    )
+                                })
+                            }
+                        </TableBody>
+                    </Table>
+                </div>
+            )}
 
         </>
-    );
+    )
 }
